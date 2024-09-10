ca408772ae92724156a042b0da573f727de5325e
Buczkó Béla <buczko.bela@burg.lo.hu>
2024-09-10 12:53:44 +0200

    Elso feladat

--- Feladat/ConsoleApp1/ConsoleApp1/Meccs.cs
+++ Feladat/ConsoleApp1/ConsoleApp1/Meccs.cs
@@ -8,5 +8,23 @@ namespace ConsoleApp1
 {
     internal class Meccs
     {
+        public int fordulo { get; set; }
+        public int hazaiVege { get; set; }
+        public int vendegVege { get; set; }
+        public int hazaiFel { get; set; }
+        public int vendegFel { get; set; }
+        public string hazai { get; set; }
+        public string vendeg { get; set; }
+
+        public Meccs(int fordulo, int hazaiVege, int vendegVege, int hazaiFel, int vendegFel, string hazai, string vendeg)
+        {
+            this.fordulo = fordulo;
+            this.hazaiVege = hazaiVege;
+            this.vendegVege = vendegVege;
+            this.hazaiFel = hazaiFel;
+            this.vendegFel = vendegFel;
+            this.hazai = hazai;
+            this.vendeg = vendeg;
+        }
     }
 }
--- Feladat/ConsoleApp1/ConsoleApp1/Program.cs
+++ Feladat/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,25 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        public static List<Meccs> meccsek = Beolvasas();
         static void Main(string[] args)
         {
+            Console.WriteLine(meccsek);
+        }
+
+        public static List<Meccs> Beolvasas() { 
+        
+            List<Meccs> meccsek = new List<Meccs>();
+
+
+            foreach (var lines in File.ReadAllLines("meccs.txt").Skip(1)) { 
+            
+                var line = lines.Split(' ');
+                meccsek.Add(new Meccs(Convert.ToInt32(line[0]), Convert.ToInt32(line[1]), Convert.ToInt32(line[2]), Convert.ToInt32(line[3]), Convert.ToInt32(line[4]), line[5], line[6] ));
+            }
+
+            return meccsek;
+
         }
     }
 }
